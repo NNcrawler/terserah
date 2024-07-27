2c815938bad9049ae6c68bb45a971eed8ba8688c
Rasyid Hakim <muhammad.hakim@go-jek.com>
2024-07-27 17:08:28 +0700

    feat: render list of food to the UI

--- makan/src/App.js
+++ makan/src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import fetchRecommendations from './api/recommendation';
 import './App.css';
 import LocationFetcher from './LocationFetcher';
 import { useGeolocation } from '@uidotdev/usehooks';
@@ -5,11 +7,24 @@ import { useGeolocation } from '@uidotdev/usehooks';
 function App() {
   const location = useGeolocation();
 
+  const [recommendations, setRecommendations] = useState([]);
+  useEffect(() => {
+    if (location.latitude && location.longitude) {
+      fetchRecommendations(location).then((response) => {
+        setRecommendations(response.data);
+      });
+    }
+  }, [location]);
+
   return (
     <div className="App">
       <div class="flex items-center justify-center h-screen">
         <div>
           <LocationFetcher location={location} />
+          {recommendations.length > 0 &&
+            recommendations.map((recommendation, i) => (
+              <p id={i}>{recommendation.name}</p>
+            ))}
         </div>
       </div>
     </div>
